d991f951375e36d88f0647c76e220a3882197ec1
innectic <innectic@gmail.com>
2016-03-29 16:52:21 -0700

    Added basic logins

--- main.py
+++ main.py
@@ -1,21 +1,56 @@
-from flask import Flask, render_template, request
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from flask import Flask, render_template
+from flask.ext.login import LoginManager, login_required, UserMixin
+
+from json import load
 
 app = Flask(__name__)
-engine = create_engine('sqlite:////data/user.db', convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
+secret = ""
+
+# Load the config
 
 
-Base = declarative_base()
-Base.query = db_session.query_property()
+def load_config():
+    with open('data/config.json') as f:
+        data = load(f)
+        secret = data['secret']
 
 
-def init_db():
-    Base.metadata.create_all(bind=engine)
+# Flask-login things
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin):
+    db = {"2Cubed": ("2Cubed", "Tech"),
+          "Paradigm": ("Para", "digm")}
+
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+
+    @classmethod
+    def get(cls, id):
+        return cls.db.get(id)
+
+
+@login_manager.request_loader
+def load_user(request):
+    token = request.headers.get('Authorization')
+
+    if token is None:
+        token = request.args.get('token')
+
+    if token is not None:
+        username, password = token.split(":")
+        user_entry = User.get(username)
+
+        if user_entry is not None:
+            user = User(user_entry[0], user_entry[1])
+
+            if user.password == password:
+                return user
+    return None
 
 
 # Main route
@@ -27,16 +62,17 @@ def index():
 # Login route
 @app.route('/login', methods=["GET", "POST"])
 def login():
-    error = None
+    # TODO
+    pass
 
-    if request.method == "POST":
-        if request.form['username'] != "f" or request.form['password'] != "f":
-            error = "Invalid credentials."
-        else:
-            return render_template('panel')
-    return render_template('login', error=error)
+
+@app.route('/logout', methods=["GET"])
+@login_required
+def logout():
+    # TODO
+    pass
 
 
 if __name__ == '__main__':
-    # init_db()
+    app.config["SECRET_KEY"] = secret
     app.run()
