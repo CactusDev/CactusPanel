a38cd3d92e100448548cb6bfd4746acabc00859b
innectic <innectic@gmail.com>
2016-03-29 14:19:45 -0700

    Added hover things and some css

--- static/js/angular/IndexMain.js
+++ static/js/angular/IndexMain.js
@@ -1,5 +1,13 @@
 var app = angular.module('IndexApp',[]);
+var showUserHover = false;
 
 app.controller('IndexController', ['$scope', function ($scope) {
   $scope.username = "Innectic";
+
+  $scope.userHover = function () {
+    showUserHover = !showUserHover;
+    $scope.showUserHover = showUserHover;
+
+    return $scope.showUserHover;
+  }
 }]);
