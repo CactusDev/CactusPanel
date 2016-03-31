60c8ab0f08217a83952629195ca32aff5f5267cd
innectic <innectic@gmail.com>
2016-03-31 00:50:49 -0700

    Added support, and refactored to Angular material for all the things

--- app/static/js/angular/IndexMain.js
+++ app/static/js/angular/IndexMain.js
@@ -1,13 +1,6 @@
-var app = angular.module('IndexApp',[]);
+var app = angular.module('IndexApp', ['ngMaterial']);
 var showUserHover = false;
 
 app.controller('IndexControl', ['$scope', function ($scope) {
   $scope.username = "Innectic";
-
-  $scope.userHover = function () {
-    showUserHover = !showUserHover;
-    $scope.showUserHover = showUserHover;
-
-    return $scope.showUserHover;
-  }
 }]);
