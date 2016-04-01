8950dae65eddbe4a48c4a352d1af393e486d2e3d
innectic <innectic@gmail.com>
2016-03-31 20:59:33 -0700

    Did lots of things

--- app/static/js/angular/IndexMain.js
+++ app/static/js/angular/IndexMain.js
@@ -1,6 +1,44 @@
 var app = angular.module('IndexApp', ['ngMaterial']);
-var showUserHover = false;
+var isPartnered = true;
+var updateMessages = false;
+var hideMessages = false;
 
 app.controller('IndexControl', ['$scope', function ($scope) {
-  $scope.username = "Innectic";
+  $scope.isPartnered = isPartnered;
+
+  // Temp setting of these
+
+  // Spam prot
+
+  $scope.messageLength = Math.floor(Math.random() * 100);
+  $scope.maxCaps = Math.floor(Math.random() * 100);
+  $scope.maxEmotes = Math.floor(Math.random() * 100);
+
+  // Messages
+
+  $scope.followText = "Thanks for the follow %user%! You rock!";
+  $scope.subText = "Thanks for the sub %user%! You rock!";
+  $scope.resubText = "Thanks for the resub %user% for %months% in a row! You rock!";
+
+  // Protection things
+
+  $scope.isProtEnabled = true;
+
+  $scope.pressedUpdateMessages = function() {
+    updateMessages = !updateMessages;
+    hideMessages = !hideMessages;
+
+    $scope.updateMessages = updateMessages;
+    $scope.shouldHideChangeText = hideMessages;
+
+    // This is where the call to the bot would go
+
+    updateMessages = !updateMessages;
+    hideMessages = !hideMessages;
+
+    $scope.updateMessages = updateMessages;
+    $scope.shouldHideChangeText = hideMessages;
+
+    // TODO: Send a request to the bot to update all the things
+  }
 }]);
