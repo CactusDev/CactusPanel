42f2d3153da47bc97c2992d4d1e7b3ddad33a2d5
RPiAwesomeness <keyboardmailtesting@gmail.com>
2016-04-02 03:03:55 -0400

    trying to fix sqlalchemy

--- app/models.py
+++ app/models.py
@@ -13,7 +13,6 @@ pwd_context = CryptContext(
 
 
 class User(UserMixin, db.Model):
-    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -67,10 +66,9 @@ class User(UserMixin, db.Model):
 
 
 class Bot(db.Model):
-    __tablename__ = "bots"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
-    owner = db.Column(db.Integer, db.ForeignKey('users.id'))
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return "<{bname} - [{bid}] {owner}>".format(
