dcb5dfb3591f8381ab9db4c0e0879058e4cac47c
RPiAwesomeness <keyboardmailtesting@gmail.com>
2016-04-02 01:13:13 -0400

    ;

--- app/auth.py
+++ app/auth.py
@@ -64,7 +64,7 @@ class BeamSignIn(OAuthSignIn):
                 "client_id": self.consumer_id,
                 "client_secret": self.consumer_secret
             },
-            decoder=lambda b: json.loads(str(b))
+            decoder=lambda b: print(b); json.loads(str(b))
         )
         print(oauth_session)
         return "FOO", "bar", "spam"
