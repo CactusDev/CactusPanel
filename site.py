0a984cff8b8bad5c14b4019688cde1f732dda074
RPiAwesomeness <keyboardmailtesting@gmail.com>
2016-04-02 06:18:26 +0000

    Fixing stuffs

--- site.py
+++ site.py
@@ -3,4 +3,4 @@ from app import app, socketio
 if __name__ == "__main__":
     # Using socketio.run instead of app.run because we need to support socketio
     # MAKE SURE TO REMOVE PORT AND DEBUG!
-    socketio.run(app, debug=True, port=8000)
+    socketio.run(app, debug=True, host="0.0.0.0", port=8000)
