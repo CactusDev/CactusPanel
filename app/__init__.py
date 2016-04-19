ea99f30023393e1ef3465e1e96b37c40169d5a9b
RPiAwesomeness <keyboardmailtesting@gmail.com>
2016-04-18 20:29:01 -0400

    Fixing stuff & things & socketio

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask.ext.login import LoginManager
 from flask.ext.socketio import SocketIO
 from flask.ext.wtf.csrf import CsrfProtect
 from flask_oauthlib.client import OAuth
+from flask.ext.assets import Environment, Bundle
+
 import eventlet
 import json
 import config
@@ -17,5 +19,26 @@ lm.login_view = "login"
 csrf_protect = CsrfProtect(app)
 socketio = SocketIO(app)
 eventlet.monkey_patch()
+assets = Environment(app)
+# Does some sort of magical magic. Makes it all one file because #MAGIC™!
+js = Bundle('js/libs/angular.min.js',
+            'js/libs/jquery.min.js',
+            'js/libs/angular-animate.min.js',
+            'js/libs/angular-aria.min.js',
+            'js/libs/angular-material.min.js',
+            'js/libs/angular-messages.min.js',
+            'js/libs/socket.io.min.js',
+            'js/angular/index.js',
+            'js/diag.js',
+            'js/socket.js',
+            'js/angular/directives.js',
+            filters='jsmin',
+            output='js/packed.js')
 
-from app import views, models
+assets.register('js_all', js)
+
+# Don't remove that. It makes EVERYTHING work.
+app.config['ASSETS_DEBUG'] = True
+# If you remove it, I will take your cat. And your dog. And your familiy.
+
+from app import views, models, socketio
---
+++ app/socketio.py
@@ -0,0 +1,12 @@
+from app import app, socketio
+
+
+@socketio.on('connection')
+def handle_message(recv):
+    print("Got connection with status: " + str(recv))
+
+
+@socketio.on('updateAlerts')
+def handle(recv):
+    print(recv)
+    socketio.emit('updateComplete')
