7d8e7f979515b7ad43ef192265e9a00e0c421371
RPiAwesomeness <keyboardmailtesting@gmail.com>
2016-04-19 21:17:59 -0400

    Updates from laptop

--- site.py
+++ site.py
@@ -1,7 +1,8 @@
-from app import app, socketio
+from app import app, socketio, config
 
 if __name__ == "__main__":
 
     # Using socketio.run instead of app.run because we need to support socketio
     # MAKE SURE TO REMOVE PORT AND DEBUG!
-    socketio.run(app, debug=True, host="0.0.0.0", port=8000)
+    print(config)
+    socketio.run(app, debug=config.debug, port=config.port, host=config.host)
