2347b9fdbb427112a1cc10a72c1a1e2be545d34b
Innectic <innectic@gmail.com>
2016-04-29 20:44:25 -0700

    Delete config.py

--- instance/config.py
+++
@@ -1,5 +0,0 @@
-DEBUG=True
-MAIL_USERNAME = "Cactusdevbeam@gmail.com"
-MAIL_PASSWORD = "lpWaOwnVlPQDo3Ox%5l8@&1D0pKThBl4rg1@r6zcSYiMrn4^Ev16In4*F05juOQ@!YWUKRC@h2A" # Sample Password
-RECIPIENTS = ["innectic@gmail.com"]
-SMTP_SERVER = "smtp.gmail.com:587"
