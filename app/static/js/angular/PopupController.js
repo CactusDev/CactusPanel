ca9a161da452e68efade617722238d3e8eadfcbc
= <keyboardmailtesting@gmail.com>
2016-05-30 02:13:32 -0400

    Yup, still dislike it

--- app/static/js/angular/PopupController.js
+++ app/static/js/angular/PopupController.js
@@ -7,186 +7,188 @@ $.ajaxSetup({
 });
 
 app.controller('PopupControl', ['$scope', '$mdDialog', '$mdMedia', function($scope, $mdDialog, $mdMedia, $timeout) {
-  $scope.status = '  ';
-  $scope.didClose = false;
-  $scope.hasEntered = false;
-  $scope.gotSupported = false;
-  $scope.addedCommand = false;
-  $scope.addingCommand = false;
-  $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
-  $scope.searchString = null;
+    $scope.status = '  ';
+    $scope.didClose = false;
+    $scope.hasEntered = false;
+    $scope.gotSupported = false;
+    $scope.addedCommand = false;
+    $scope.addingCommand = false;
+    $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+    $scope.searchString = null;
 
-  $scope.searchSupport = function(e) {
-      if (e.keyCode == 13) {
-          if ($scope.searchString) {
-              var request = $scope.retrieveTickets(
-                  'POST',
-                  { search_string: $scope.searchString }
-              )
-          } else {
-              var request = $scope.retrieveTickets();
-          }
+    $scope.searchSupport = function(e) {
+        if (e.keyCode == 13) {
+            if ($scope.searchString) {
+                var request = $scope.retrieveTickets(
+                    'POST',
+                    {
+                        search_string: $scope.searchString
+                    }
+                )
+            } else {
+                var request = $scope.retrieveTickets();
+            }
 
-          request.done(function(data) {
-              console.log("data:");
-              console.log(data);
-              $scope.$apply(function() {
-                  $scope.tickets = [];
+            request.done(function(data) {
+                console.log("data:");
+                console.log(data);
+                $scope.$apply(function() {
+                    $scope.tickets = [];
 
-                  for (i = 0; i < data.length; i++) {
-                      $scope.tickets.push(data[i]);
-                  }
-              });
-            //   var diff = _.differenceBy(data, $scope.tickets, (item, key, a) => item.id);
-            //   $scope.$apply(function() {
-            //       for (ticket in diff) {
-            //           $scope.tickets.push(diff[ticket]);
-            //       }
-            //   });
-              console.log("$scope.tickets:")
-              console.log($scope.tickets);
-          });
-      }
-  }
+                    for (i = 0; i < data.length; i++) {
+                        $scope.tickets.push(data[i]);
+                    };
+                });
+                //   var diff = _.differenceBy(data, $scope.tickets, (item, key, a) => item.id);
+                //   $scope.$apply(function() {
+                //       for (ticket in diff) {
+                //           $scope.tickets.push(diff[ticket]);
+                //       }
+                //   });
+                console.log("$scope.tickets:")
+                console.log($scope.tickets);
+            });
+        }
+    }
 
-  $scope.retrieveTickets = function(type, data) {
-      if (data == undefined || data == '') {
-          var type = 'GET';
-      }
+    $scope.retrieveTickets = function(type, data) {
+        if (data == undefined || data == '') {
+            var type = 'GET';
+        }
 
-      var req = $.ajax({
-          url: '/support/list',
-          type: type,
-          data: JSON.stringify(data),
-          contentType: 'application/json'
-      });
+        var req = $.ajax({
+            url: '/support/list',
+            type: type,
+            data: JSON.stringify(data),
+            contentType: 'application/json'
+        });
 
-      return req;
-  }
+        return req;
+    }
 
-  $scope.showCreate = function(ev) {
-    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+    $scope.showCreate = function(ev) {
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
 
-    $mdDialog.show({
-      controller: DialogController,
-      templateUrl: '/support/create',
-      parent: angular.element(document.body),
-      targetEvent: ev,
-      clickOutsideToClose: true,
-      fullscreen: useFullScreen
-    });
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: '/support/create',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            fullscreen: useFullScreen
+        });
 
-    $scope.$watch(function() {
-      return $mdMedia('xs') || $mdMedia('sm');
-    }, function(wantsFullScreen) {
-      $scope.customFullscreen = (wantsFullScreen === true);
-    });
+        $scope.$watch(function() {
+            return $mdMedia('xs') || $mdMedia('sm');
+        }, function(wantsFullScreen) {
+            $scope.customFullscreen = (wantsFullScreen === true);
+        });
 
-  };
+    };
 
-  $scope.showRespond = function(ev) {
-    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+    $scope.showRespond = function(ev) {
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
 
-    $mdDialog.show({
-      controller: DialogController,
-      templateUrl: '/support/respond',
-      parent: angular.element(document.body),
-      targetEvent: ev,
-      clickOutsideToClose: true,
-      fullscreen: useFullScreen
-    });
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: '/support/respond',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            fullscreen: useFullScreen
+        });
 
-    $scope.$watch(function() {
-      return $mdMedia('xs') || $mdMedia('sm');
-    }, function(wantsFullScreen) {
-      $scope.customFullscreen = (wantsFullScreen === true);
-    });
+        $scope.$watch(function() {
+            return $mdMedia('xs') || $mdMedia('sm');
+        }, function(wantsFullScreen) {
+            $scope.customFullscreen = (wantsFullScreen === true);
+        });
 
-  };
+    };
 
-  $scope.showAddCommand = function(ev) {
-    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+    $scope.showAddCommand = function(ev) {
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
 
-    $mdDialog.show({
-      controller: CommandController,
-      templateUrl: '/command/create',
-      parent: angular.element(document.body),
-      targetEvent: ev,
-      clickOutsideToClose: true,
-      fullscreen: useFullScreen
-    });
+        $mdDialog.show({
+            controller: CommandController,
+            templateUrl: '/command/create',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            fullscreen: useFullScreen
+        });
 
-    $scope.$watch(function() {
-      return $mdMedia('xs') || $mdMedia('sm');
-    }, function(wantsFullScreen) {
-      $scope.customFullscreen = (wantsFullScreen === true);
-    });
+        $scope.$watch(function() {
+            return $mdMedia('xs') || $mdMedia('sm');
+        }, function(wantsFullScreen) {
+            $scope.customFullscreen = (wantsFullScreen === true);
+        });
 
-  }
+    }
 }]);
 
 function DialogController($scope, $mdDialog) {
-  $scope.hide = function() {
-    $mdDialog.hide();
-  };
+    $scope.hide = function() {
+        $mdDialog.hide();
+    };
 
-  $scope.cancel = function() {
-    $mdDialog.cancel();
-  };
+    $scope.cancel = function() {
+        $mdDialog.cancel();
+    };
 
-  $scope.submit = function(e) {
-    if (e.which === 13) {
-      if (e.button != true) {
-        e.preventDefault();
-      }
-      if ($scope.issue == undefined || $scope.issue == '' ||
-        $scope.details == undefined || $scope.details == '') {
-        console.log("BIG BAD EXPLOSIONS!")
-      }
-      $scope.gotSupported = true;
+    $scope.submit = function(e) {
+        if (e.which === 13) {
+            if (e.button != true) {
+                e.preventDefault();
+            }
+            if ($scope.issue == undefined || $scope.issue == '' ||
+                $scope.details == undefined || $scope.details == '') {
+                console.log("BIG BAD EXPLOSIONS!")
+            }
+            $scope.gotSupported = true;
 
-      $.ajax({
-          url: '/support/create',
-          type: 'POST',
-          data: JSON.stringify({
-            issue: $scope.issue,
-            details: $scope.details,
-          }),
-          contentType: 'application/json'
-        })
-        .done(function(request) {
-          request = JSON.stringify(request);
-          console.log(request);
-          if (request.success) {
-            console.log("SUCCESS!")
-          }
-        });
+            $.ajax({
+                    url: '/support/create',
+                    type: 'POST',
+                    data: JSON.stringify({
+                        issue: $scope.issue,
+                        details: $scope.details,
+                    }),
+                    contentType: 'application/json'
+                })
+                .done(function(request) {
+                    request = JSON.stringify(request);
+                    console.log(request);
+                    if (request.success) {
+                        console.log("SUCCESS!")
+                    }
+                });
+        }
     }
-  }
 }
 
 function ConfirmedController($scope, $mdDialog) {
-  $scope.cancel = function() {
-    $scope.didClose = true;
+    $scope.cancel = function() {
+        $scope.didClose = true;
 
-    $mdDialog.cancel();
-  };
+        $mdDialog.cancel();
+    };
 }
 
 function CommandController($scope, $mdDialog, $timeout) {
-  $scope.showCreate = true;
-  $scope.cancel = function() {
-    $mdDialog.cancel();
-  };
+    $scope.showCreate = true;
+    $scope.cancel = function() {
+        $mdDialog.cancel();
+    };
 
-  $scope.addCommand = function() {
-    $scope.showCreate = false;
+    $scope.addCommand = function() {
+        $scope.showCreate = false;
 
-    $timeout(function() {
-      $scope.addedCommand = true;
-      $scope.addingCommand = false;
-    }, 2000);
+        $timeout(function() {
+            $scope.addedCommand = true;
+            $scope.addingCommand = false;
+        }, 2000);
 
-    $scope.addingCommand = true;
-  }
+        $scope.addingCommand = true;
+    }
 }
