55ba825baa399365c635ba74ea586516cc6a2418
Innectic <innectic@gmail.com>
2016-08-16 19:53:58 -0700

    Begin rewrite.
    Create basic stuff using angular cli

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Cactus-CP
-Here's our neat little control panel!
+
+CactusBot Control Panel.
 
 All of our emojis for the site and the Cactus that we use, all come from [EmojiOne](http://emojione.com/)
--- run.py
+++
@@ -1,8 +0,0 @@
-"""Main code for running the web server."""
-
-from app import app
-
-if __name__ == "__main__":
-    app.run(debug=app.config["DEBUG"],
-            port=app.config["PORT"],
-            host=app.config["HOST"])
