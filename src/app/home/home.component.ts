389f3c9d0045f9d45d1da9d751bac508ebf42582
Innectic <innectic@gmail.com>
2016-10-14 15:23:52 -0700

    Change quick actions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,28 @@ import { Component } from "@angular/core";
     styleUrls: ["home.component.less"]
 })
 export class HomeComponent {
+    actions: Object[];
+
     constructor() {
+        this.actions = [
+            {
+                "name": "Add a user as moderator",
+                "icon": "verified_user",
+                "subtext": "Make someone a chat moderator.",
+                "route": ""
+            },
+            {
+                "name": "Ban a user",
+                "icon": "gavel",
+                "subtext": "Ban a user from chat.",
+                "route": ""
+            },
+            {
+                "name": "Timeout a user",
+                "icon": "gavel",
+                "subtext": "Prevent a user from chatting for an amount of time",
+                "route": ""
+            }
+        ];
     }
 }
