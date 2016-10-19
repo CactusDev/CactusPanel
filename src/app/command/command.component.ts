73cb9badf2f8c487200e192d105e81086fd34e5c
Innectic <innectic@gmail.com>
2016-10-18 23:23:52 -0700

    More UI changes

--- src/app/command/command.component.ts
+++ src/app/command/command.component.ts
@@ -6,10 +6,9 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["command.component.less"]
 })
 export class CommandComponent implements OnInit {
-
     isCreatingCommand: boolean = false;
 
     ngOnInit() { }
 
-    constuctor() { }
+    constructor() { }
 }
