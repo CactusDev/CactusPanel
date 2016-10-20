61f5fe8c0d18918643224fe181d684cf803c6e11
Innectic <innectic@gmail.com>
2016-10-19 20:33:58 -0700

    Remove actions from home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,30 +6,7 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["home.component.less"]
 })
 export class HomeComponent implements OnInit {
-    actions: Object[];
-
-    ngOnInit() {
-        this.actions = [
-            {
-                "name": "Add a user as moderator",
-                "icon": "verified_user",
-                "subtext": "Make someone a chat moderator.",
-                "route": ""
-            },
-            {
-                "name": "Ban a user",
-                "icon": "gavel",
-                "subtext": "Ban a user from chat.",
-                "route": ""
-            },
-            {
-                "name": "Timeout a user",
-                "icon": "gavel",
-                "subtext": "Prevent a user from chatting for an amount of time",
-                "route": ""
-            }
-        ];
-    }
+    ngOnInit() { }
 
     constructor() { }
 }
