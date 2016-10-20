c23c7b9b7d9fab1d685a7be62e4f0ec7dad00462
Innectic <innectic@gmail.com>
2016-10-19 20:37:43 -0700

    Add new home text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["home.component.less"]
 })
 export class HomeComponent implements OnInit {
+    name: string = "Devacus";
+
     ngOnInit() { }
 
     constructor() { }
