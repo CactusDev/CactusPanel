0a8705a86f4c62c01480507786191d479fe9651b
Innectic <innectic@gmail.com>
2017-10-16 01:54:32 -0700

    Begin on React rewrite

---
+++ src/components/Hello.tsx
@@ -0,0 +1,32 @@
+
+import * as React from "react";
+
+import { Drawer } from "react-toolbox/lib/drawer";
+import { Button } from "react-toolbox/lib/button";
+
+export class Hello extends React.Component {
+    state = {
+        active: false,
+    };
+
+    toggle = () => {
+        this.setState({ active: !this.state.active });
+    }
+
+    render() {
+        return (
+            <div>
+                <Button label='Show Drawer' raised accent onClick={this.toggle} />
+                <Drawer active={this.state.active} onOverlayClick={this.toggle} className="navigation_container">
+                    <h3>Navigation</h3>
+
+                    <Button raised accent label="Dashboard" className="navigation_button" />
+                    <Button raised accent label="Commands" className="navigation_button" />
+                    <Button raised accent label="Quotes" className="navigation_button" />
+                    <Button raised accent label="Event Alerts" className="navigation_button" />
+                    <Button raised accent label="Settings" className="navigation_button" />
+                </Drawer>
+            </div>
+        );
+    }
+}
